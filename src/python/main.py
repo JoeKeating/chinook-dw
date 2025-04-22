e385393a91b28cd0f5448e64d5c73d8b07485654
Joe Keating <2josephpkeating@gmail.com>
2025-04-22 10:16:56 -0400

    Refactor: Removed chinook_crendentials.py

--- src/python/main.py
+++ src/python/main.py
@@ -1,10 +1,7 @@
 import json
 import logging
 import yaml
-from src.python.config.chinook_config import (
-    chinook_db_config,
-    get_chinook_loader_secret,
-)
+from boto3 import Session
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 import src.python.chinook_loader as chinook_loader
@@ -17,25 +14,53 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def open_create_engine(db_config: dict) -> Engine | bool:
-    """Open a connection to Snowflake"""
+def get_chinook_loader_secret() -> dict:
+    try:
+        session = Session(profile_name="chinook-loader-secret-reader")
+        client = session.client(service_name="secretsmanager", region_name="us-east-2")
+    except Exception as e:
+        logging.error(
+            f"Function get_chinook_loader_secret: Error occurred establishing {e}"
+        )
+        return {}
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId="chinook_loader_secret"
+        )
+        parsed_secrets = json.loads(get_secret_value_response["SecretString"])
+        logging.info(f"Function get_chinook_loader_secret: Completed successfully.")
+        return parsed_secrets
+    except Exception as e:
+        logging.error(
+            f"Function get_chinook_loader_secret: Error occurred establishing {e}"
+        )
+        return {}
 
-    # Get secrets from AWS Secrets Manager
+
+def build_chinook_dw_connection_string(parsed_yaml_json: dict) -> str:
+    """Get chinook connect data from table_load.yaml"""
     secrets = get_chinook_loader_secret()
-
     db_user = secrets["username"]
     db_password = secrets["password"]
-    db_account = db_config["DB_ACCOUNT"]
-    db_database = db_config["DB_DATABASE"]
-    db_schema = db_config["DB_SCHEMA"]
-    db_role = db_config["DB_ROLE"]
-    db_warehouse = db_config["DB_WAREHOUSE"]
+    db_account = parsed_yaml_json["connection_string"]["account"]
+    db_database = parsed_yaml_json["connection_string"]["database"]
+    db_schema = parsed_yaml_json["connection_string"]["schema"]
+    db_role = parsed_yaml_json["connection_string"]["role"]
+    db_warehouse = parsed_yaml_json["connection_string"]["warehouse"]
+    db_timezone = parsed_yaml_json["connection_string"]["timezone"]
+
+    chinook_dw_connection_string = (
+        f"snowflake://{db_user}:{db_password}@{db_account}/"
+        f"{db_database}/{db_schema}?role={db_role}&warehouse={db_warehouse}&timezone={db_timezone}"
+    )
+    return chinook_dw_connection_string
+
+
+def open_create_engine(connect_string: str) -> Engine | bool:
+    """Open a connection to Snowflake"""
+
     try:
-        engine = create_engine(
-            f"snowflake://{db_user}:{db_password}@{db_account}/"
-            f"{db_database}/{db_schema}?role={db_role}&warehouse={db_warehouse}&\
-                timezone=America/New_York"
-        )
+        engine = create_engine(connect_string)
         return engine
 
     except Exception as e:
@@ -61,7 +86,7 @@ def read_table_load_yaml(file_path: str) -> dict:
     """Read in table_load.yaml"""
 
     try:
-        with open(file_path,'r') as in_file:
+        with open(file_path, "r") as in_file:
             parsed_config_data = yaml.safe_load(in_file)
             return parsed_config_data
 
@@ -75,39 +100,26 @@ def read_table_load_yaml(file_path: str) -> dict:
 
 def main():
 
-    chinook_data_file_path = r'/Users/jpk/Projects/chinook-dw/data/ChinookData.json'
-    table_load_file_path = r'/Users/jpk/Projects/chinook-dw/src/python/config/table_load.yaml'
-    
+    chinook_data_file_path = r"/Users/jpk/Projects/chinook-dw/data/ChinookData.json"
+    table_load_file_path = (
+        r"/Users/jpk/Projects/chinook-dw/src/python/config/table_load.yaml"
+    )
+
     chinook_data = read_chinook_data(chinook_data_file_path)
     parsed_yaml_json = read_table_load_yaml(table_load_file_path)
-    table_list = chinook_loader.get_table_list(parsed_yaml_json, 'landing')
-    engine = open_create_engine(chinook_db_config)
+    table_list = chinook_loader.get_table_list(parsed_yaml_json, "landing")
+    connect_string = build_chinook_dw_connection_string(parsed_yaml_json)
+    engine = open_create_engine(connect_string)
+    print(connect_string)
 
-    
-  
     for table in table_list:
-        table_data = chinook_loader.build_load_data(chinook_data,\
-                                                        parsed_yaml_json, \
-                                                        'landing',
-                                                        table,
-                                                        ) 
-        chinook_loader.load_chinook_data(table_data, 'landing', table, engine)
-
-        
-
-    
-
-    # chinook_loader.load__genre(chinook_data, engine)
-    # chinook_loader.load__media_type(chinook_data, engine)
-    # chinook_loader.load__artist(chinook_data, engine)
-    # chinook_loader.load__album(chinook_data, engine)
-    # chinook_loader.load__track(chinook_data, engine)
-    # chinook_loader.load__employee(chinook_data, engine)
-    # chinook_loader.load__customer(chinook_data, engine)
-    # chinook_loader.load__invoice(chinook_data, engine)
-    # chinook_loader.load__invoice_line(chinook_data, engine)
-    # chinook_loader.load__playlist(chinook_data, engine)
-    # chinook_loader.load__playlist_track(chinook_data, engine)
+        table_data = chinook_loader.build_load_data(
+            chinook_data,
+            parsed_yaml_json,
+            "landing",
+            table,
+        )
+        chinook_loader.load_chinook_data(table_data, "landing", table, engine)
 
 
 if __name__ == "__main__":
